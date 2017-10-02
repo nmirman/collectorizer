10394391c9ea016a001577149dbcae406c069142
Nathan Mirman <nem46@cornell.edu>
2017-10-02 11:20:58 -0400

    Expanded analysis.

--- recommender.py
+++ recommender.py
@@ -7,9 +7,11 @@ import random
 
 class Recommender:
     
-    def __init__(self, ratings, kind, ratings_weighted=None):
+    def __init__(self, ratings, kind, ratings_weighted=None, user_stats=None, item_stats=None):
         self.ratings = ratings
         self.ratings_weighted = ratings_weighted
+        self.user_stats = user_stats
+        self.item_stats = item_stats
         self.kind = kind
         self.nusers = ratings.shape[0]
         self.nitems = ratings.shape[1]
@@ -61,12 +63,25 @@ class Recommender:
 
         print('done!')
 
+    def get_priceweight(self, user, item, k=1):
 
-    def get_user_recs(self, user):
+        pfrac =  float(self.item_stats[item, 'pricerangelow']) / self.user_stats.loc[user, 'price_max']
+
+        if pfrac == None or (pfrac < 1):
+            weight = 1
+        else:
+            weight = math.exp( -k*pfrac )
+
+        return weight
+
+
+    def get_user_recs(self, user, pweight=-1):
 
         rec_dict = {}
         for i in range(self.nitems):
             rec_dict[i] = self.user_predictions[user,i]
+            if pweight != -1:
+                rec_dict[i] *= self.get_priceweight(user, i, pweight)
 
         # sort the list in order of decreasing predicted ratings
         recs_sorted = sorted(rec_dict.items(), key=operator.itemgetter(1), reverse=True)
@@ -142,27 +157,46 @@ def get_roc(pred, actual, nrecitems):
         for i in ind:
             pred[u][i] = 1
 
-    pred = pred.flatten()
-    actual = actual.flatten()
+    #pred = pred.flatten()
+    #actual = actual.flatten()
 
     tp = 0.0
     fp = 0.0
     tn = 0.0
     fn = 0.0
+    precision = 0.0
+    recall = 0.0
     print('calculating roc...')
-    for i in range(len(pred)):
-        if( pred[i] == 0 ):
-            if( actual[i] == 1 ):
-                fn += 1.0
-            else:
-                tn += 1.0
-        elif( pred[i] == 1):
-            if( actual[i] == 1 ):
-                tp += 1.0
-            else:
-                fp += 1.0
+    #for i in range(len(pred)):
+    for i in range(pred.shape[0]):
+
+        tpu = 0
+        fnu = 0
+        fpu = 0
+
+        for j in range(pred.shape[1]):
+            pred_value = pred[i,j]
+            actual_value = actual[i,j]
+            if( pred_value == 0 ):
+                if( actual_value == 1 ):
+                    fn += 1.0
+                    fnu += 1.0
+                else:
+                    tn += 1.0
+            elif( pred_value == 1):
+                if( actual_value == 1 ):
+                    tp += 1.0
+                    tpu += 1.0
+                else:
+                    fp += 1.0
+                    fpu += 1.0
+
+        if tpu + fpu != 0:
+            precision += tpu/(tpu+fpu)
+        if tpu + fnu != 0:
+            recall += tpu/(tpu+fnu)
     
-    return tp, fp, tn, fn
+    return tp, fp, tn, fn, precision/pred.shape[0], recall/pred.shape[0]
 
 
 def run_validation(ratings, kind, nfolds, nrecitems=10):
@@ -172,6 +206,8 @@ def run_validation(ratings, kind, nfolds, nrecitems=10):
     fp_sum = 0
     tn_sum = 0
     fn_sum = 0
+    precision_sum = 0
+    recall_sum = 0
     for fold in range(nfolds):
         print('... fold', fold+1, '/', nfolds)
 
@@ -200,15 +236,26 @@ def run_validation(ratings, kind, nfolds, nrecitems=10):
             nans = np.isnan(preds)
             preds[nans] = 0
 
-        tp, fp, tn, fn = get_roc(preds, test, nrecitems)
+            # remove items already selected by user
+            print('removing items already selected by user...')
+            bindices_nonzero = (train != 0)
+            preds[bindices_nonzero] = 0
+
+        tp, fp, tn, fn, precision, recall = get_roc(preds, test, nrecitems)
         tp_sum += tp
         fp_sum += fp
         tn_sum += tn
         fn_sum += fn
+        precision_sum += precision
+        recall_sum += recall
+
+        break
 
     print('TP, FP, TN, FN = ', tp_sum, fp_sum, tn_sum, fn_sum)
     print('Precision:', tp_sum/(tp_sum+fp_sum) )
     print('True positive rate:', tp_sum/(tp_sum+fn_sum) )
     print('False positive rate:', fp_sum/(fp_sum+tn_sum) )
+    print('User precision:', precision_sum )
+    print('User recall:', recall_sum )
 
-    return tp_sum, fp_sum, tn_sum, fn_sum
+    return tp_sum, fp_sum, tn_sum, fn_sum, precision_sum, recall_sum
